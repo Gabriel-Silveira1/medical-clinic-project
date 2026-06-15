1f56cf7b866f1f32f6a4e02627da00b7a1c83caa
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-15 16:00:17 -0300

    DoctorRepository created

---
+++ src/main/java/com/gabrielsilveira/clinica/repositories/DoctorRepository.java
@@ -0,0 +1,10 @@
+package com.gabrielsilveira.clinica.repositories;
+
+import com.gabrielsilveira.clinica.entities.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    
+}
