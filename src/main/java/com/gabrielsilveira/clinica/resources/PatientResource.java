7bc7e82a24052c010dfb5739b6b6e9123c29c3f5
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-19 18:32:23 -0300

    CRUD complete for Patient

--- src/main/java/com/gabrielsilveira/clinica/resources/PatientResource.java
+++ src/main/java/com/gabrielsilveira/clinica/resources/PatientResource.java
@@ -4,10 +4,10 @@ import com.gabrielsilveira.clinica.entities.Patient;
 import com.gabrielsilveira.clinica.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +21,33 @@ public class PatientResource {
     public ResponseEntity<List<Patient>> findAll() {
         List<Patient> patients = patientService.findAll();
         return ResponseEntity.ok().body(patients);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Patient> findById(@PathVariable Long id) {
+        Patient obj = patientService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Patient> insert(@RequestBody Patient obj) {
+        obj = patientService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        patientService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Patient> update(@PathVariable Long id, @RequestBody Patient obj) {
+        obj = patientService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- src/main/java/com/gabrielsilveira/clinica/services/PatientService.java
+++ src/main/java/com/gabrielsilveira/clinica/services/PatientService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PatientService {
@@ -15,5 +16,30 @@ public class PatientService {
 
     public List<Patient> findAll() {
         return patientRepository.findAll();
+    }
+
+    public Patient findById(Long id) {
+        Optional<Patient> obj = patientRepository.findById(id);
+        return obj.get();
+    }
+
+    public Patient insert (Patient obj) {
+        return patientRepository.save(obj);
+    }
+
+    public void delete (Long id) {
+        patientRepository.deleteById(id);
+    }
+
+    public Patient update (Long id, Patient obj) {
+        Patient entity = patientRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return patientRepository.save(entity);
+    }
+
+    public void updateData(Patient entity, Patient obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
