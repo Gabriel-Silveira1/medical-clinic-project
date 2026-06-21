8b35296f51aaf32977d7837711feb65650b81746
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-20 22:14:56 -0300

    Exception handling for findById, update and delete

--- src/main/java/com/gabrielsilveira/clinica/services/PatientService.java
+++ src/main/java/com/gabrielsilveira/clinica/services/PatientService.java
@@ -2,7 +2,12 @@ package com.gabrielsilveira.clinica.services;
 
 import com.gabrielsilveira.clinica.entities.Patient;
 import com.gabrielsilveira.clinica.repositories.PatientRepository;
+import com.gabrielsilveira.clinica.services.exceptions.DatabaseException;
+import com.gabrielsilveira.clinica.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,7 +25,7 @@ public class PatientService {
 
     public Patient findById(Long id) {
         Optional<Patient> obj = patientRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public Patient insert (Patient obj) {
@@ -28,13 +33,23 @@ public class PatientService {
     }
 
     public void delete (Long id) {
-        patientRepository.deleteById(id);
+        try {
+            patientRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public Patient update (Long id, Patient obj) {
-        Patient entity = patientRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return patientRepository.save(entity);
+        try {
+            Patient entity = patientRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return patientRepository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     public void updateData(Patient entity, Patient obj) {
