a8f04a0ef1cd461f560d0905a32f6d2c8302ede2
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-25 17:06:51 -0300

    CRUD and Exceptions on Doctor entity

--- src/main/java/com/gabrielsilveira/clinica/resources/DoctorResource.java
+++ src/main/java/com/gabrielsilveira/clinica/resources/DoctorResource.java
@@ -4,10 +4,10 @@ import com.gabrielsilveira.clinica.entities.Doctor;
 import com.gabrielsilveira.clinica.services.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,5 +20,33 @@ public class DoctorResource {
     public ResponseEntity<List<Doctor>> findAll() {
         List<Doctor> doctors = doctorService.findAll();
         return ResponseEntity.ok().body(doctors);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Doctor> findById(@PathVariable Long id) {
+        Doctor obj = doctorService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Doctor> insert(@RequestBody Doctor obj) {
+        obj = doctorService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        doctorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity<Doctor> update(@PathVariable Long id, @RequestBody Doctor obj) {
+        obj = doctorService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- src/main/java/com/gabrielsilveira/clinica/services/DoctorService.java
+++ src/main/java/com/gabrielsilveira/clinica/services/DoctorService.java
@@ -2,10 +2,16 @@ package com.gabrielsilveira.clinica.services;
 
 import com.gabrielsilveira.clinica.entities.Doctor;
 import com.gabrielsilveira.clinica.repositories.DoctorRepository;
+import com.gabrielsilveira.clinica.services.exceptions.DatabaseException;
+import com.gabrielsilveira.clinica.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DoctorService {
@@ -14,5 +20,41 @@ public class DoctorService {
 
     public List<Doctor> findAll() {
         return doctorRepository.findAll();
+    }
+
+    public Doctor findById(Long id) {
+        Optional<Doctor> obj = doctorRepository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Doctor insert(Doctor obj) {
+        return doctorRepository.save(obj);
+    }
+
+    public void delete (Long id) {
+        try {
+            doctorRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Doctor update(Long id, Doctor obj) {
+        try {
+            Doctor doctor = doctorRepository.getReferenceById(id);
+            updateData(doctor, obj);
+            return doctorRepository.save(doctor);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Doctor doctor, Doctor obj) {
+        doctor.setName(obj.getName());
+        doctor.setCrm(obj.getCrm());
+        doctor.setEmail(obj.getEmail());
+        doctor.setPhone(obj.getPhone());
     }
 }
