32d41c1d6392ebe0dcd53f4f1c4a730a6c5125b0
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-25 20:51:25 -0300

    CRUD and Exceptions on Specialty entity

--- src/main/java/com/gabrielsilveira/clinica/resources/SpecialtyResource.java
+++ src/main/java/com/gabrielsilveira/clinica/resources/SpecialtyResource.java
@@ -4,10 +4,10 @@ import com.gabrielsilveira.clinica.entities.Specialty;
 import com.gabrielsilveira.clinica.services.SpecialtyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +21,33 @@ public class SpecialtyResource {
     public ResponseEntity<List<Specialty>> findAll() {
         List<Specialty> specialties = specialtyService.findAll();
         return ResponseEntity.ok().body(specialties);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Specialty> findById(@PathVariable Long id) {
+        Specialty specialty = specialtyService.findById(id);
+        return ResponseEntity.ok().body(specialty);
+    }
+
+    @PostMapping
+    public ResponseEntity<Specialty> insert (@RequestBody Specialty obj) {
+        obj =  specialtyService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        specialtyService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Specialty> update(@PathVariable Long id, @RequestBody Specialty obj) {
+        obj = specialtyService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- src/main/java/com/gabrielsilveira/clinica/services/SpecialtyService.java
+++ src/main/java/com/gabrielsilveira/clinica/services/SpecialtyService.java
@@ -2,10 +2,16 @@ package com.gabrielsilveira.clinica.services;
 
 import com.gabrielsilveira.clinica.entities.Specialty;
 import com.gabrielsilveira.clinica.repositories.SpecialtyRepository;
+import com.gabrielsilveira.clinica.services.exceptions.DatabaseException;
+import com.gabrielsilveira.clinica.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SpecialtyService {
@@ -15,5 +21,39 @@ public class SpecialtyService {
 
     public List<Specialty> findAll() {
         return specialtyRepository.findAll();
+    }
+
+    public Specialty findById(Long id) {
+        Optional<Specialty> obj = specialtyRepository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Specialty insert(Specialty obj) {
+        return specialtyRepository.save(obj);
+    }
+
+    public void delete (Long id) {
+        try {
+            specialtyRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Specialty update(Long id, Specialty obj) {
+        try {
+            Specialty specialty = specialtyRepository.getReferenceById(id);
+            updateData(specialty, obj);
+            return specialtyRepository.save(specialty);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Specialty specialty, Specialty obj) {
+        specialty.setName(obj.getName());
+        specialty.setDescription(obj.getDescription());
     }
 }
